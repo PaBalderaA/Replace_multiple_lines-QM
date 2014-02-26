d1b62ade2abde2b42d641fadffe01dce69772e03
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2014-02-26 01:17:26 -0500

    Refactoring util/main.py

--- util/main.py
+++ util/main.py
@@ -1,12 +1,10 @@
 import os
 from unipath import Path
 
+
 BASE_PATH = Path(os.path.abspath(__file__)).ancestor(2)
 DATA_PATH = BASE_PATH.child('test_data')
 NEW_DATA_PATH = BASE_PATH.child('test_new_data')
-filenames = os.listdir(str(DATA_PATH))
-
-text_to_append = BASE_PATH.child('util').child('to_replace.txt')
 
 
 def read_file(filepath):
@@ -18,15 +16,16 @@ def read_file(filepath):
     except:
         print 'Error'
 
+text_to_append = BASE_PATH.child('util').child('to_replace.txt')
 text_to_append = read_file(text_to_append)
 common = "0  1"
 
+filenames = os.listdir(DATA_PATH)
 for filename in filenames:
     flag = 0
     try:
         test = DATA_PATH + '/' + filename
         newtest = NEW_DATA_PATH + '/' + filename
-
         with open(test) as old, open(newtest, 'w') as new:
             for line in old:
                 if common in line:
@@ -38,6 +37,5 @@ for filename in filenames:
                         break
                     else:
                         new.write(line)
-
     except:
         print 'Error'
