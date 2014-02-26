1f9754294eb178ad99deddf76b5d9ce17e65a44d
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2014-02-26 00:47:01 -0500

    Append 'to_replace.txt' to all test_data files after '0   1' patern

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 Replace_multiple_lines-QM
 =========================
 
-Replace multiple lines (coordinates) for quantum mechanics' inputs files. 
+Replace multiple lines (coordinates) for quantum mechanics' inputs files.
+
+To run
+======
+
+```bash
+$ python util/main.py
+```
--- util/main.py
+++ util/main.py
@@ -3,15 +3,41 @@ from unipath import Path
 
 BASE_PATH = Path(os.path.abspath(__file__)).ancestor(2)
 DATA_PATH = BASE_PATH.child('test_data')
+NEW_DATA_PATH = BASE_PATH.child('test_new_data')
 filenames = os.listdir(str(DATA_PATH))
 
-for filename in filenames:
+text_to_append = BASE_PATH.child('util').child('to_replace.txt')
 
+
+def read_file(filepath):
     try:
-        path = DATA_PATH + '/' + filename
-        print path
-        #f = open(path, 'rw')
-        #f.write()
-        #f.close()
+        f = open(filepath, 'rU')
+        text = f.read()
+        f.close()
+        return text
+    except:
+        print 'Error'
+
+text_to_append = read_file(text_to_append)
+common = "0  1"
+
+for filename in filenames:
+    flag = 0
+    try:
+        test = DATA_PATH + '/' + filename
+        newtest = NEW_DATA_PATH + '/' + filename
+
+        with open(test) as old, open(newtest, 'w') as new:
+            for line in old:
+                if common in line:
+                    flag = 1
+                    new.write(line)
+                else:
+                    if flag == 1:
+                        new.write(text_to_append)
+                        break
+                    else:
+                        new.write(line)
+
     except:
         print 'Error'
